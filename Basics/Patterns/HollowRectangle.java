cb56a85d5adf87aed8f8a9840af7aea9aac0aebd
PatukuriMeghana78 <patukurimeghana@gmail.com>
2025-08-03 18:42:56 +0530

    Solid Rectangle

--- Basics/Patterns/HollowRectangle.java
+++ Basics/Patterns/HollowRectangle.java
@@ -1,9 +1,7 @@
-public class SolidRectangle{
+public class HollowRectangle{
     public static void main(String[] args) {
-        int a = 4;
-        int b = 5;
-        for(int i = 0; i < a; i++){
-            for (int j = 0; j < b; j++){
+        for(int i = 0; i < 4; i++){
+            for (int j = 0; j < 5; j++){
                 System.out.print("*");
             }
             System.out.println();
