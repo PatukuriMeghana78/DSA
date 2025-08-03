c7088683c5b424e90072add2c4d975d460186966
PatukuriMeghana78 <patukurimeghana@gmail.com>
2025-08-03 18:52:23 +0530

    Hollow Rectangle

--- Basics/Patterns/HollowRectangle.java
+++ Basics/Patterns/HollowRectangle.java
@@ -1,8 +1,15 @@
 public class HollowRectangle{
     public static void main(String[] args) {
-        for(int i = 0; i < 4; i++){
-            for (int j = 0; j < 5; j++){
+        int a = 4;
+        int b = 5;
+        for(int i = 1; i <= a; i++){
+            for (int j = 1; j <= b; j++){
+                if(i == 1 || i == a || j == 1 || j == b){
                 System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
             }
             System.out.println();
         }
